c9c7bb82c004a0881d9cf4b23834cf237c6466ea
Jaume <jaume.montagut.i.guix@gmail.com>
2018-05-15 18:03:29 +0200

    No more wiggle on enemy front turret
    Follows the spaceship as smooth as it gets

--- LastResort/Enemy_FrontTurret.cpp
+++ LastResort/Enemy_FrontTurret.cpp
@@ -13,14 +13,14 @@ Enemy_FrontTurret::Enemy_FrontTurret(int x, int y, POWERUP_TYPE pu_t) : Enemy(x,
 	//Collider------------------------------------------------
 	collider = App->collision->AddCollider({ 0, 0, 32, 16 }, COLLIDER_TYPE::COLLIDER_ENEMY, (Module*)App->enemies);
 	//Positions-----------------------------------------------
-	initialX = x - App->stage05->spawnPos.x;//We get the x position
-	initialY = y - App->stage05->spawnPos.y;//We get the y position
+	initialX = x - (int)App->stage05->spawnPos.x;//We get the x position
+	initialY = y - (int)App->stage05->spawnPos.y;//We get the y position
 }
 
 void Enemy_FrontTurret::Move()
 {
 	//We move the turret with the background
-	position.x = initialX + App->stage05->spawnPos.x;
-	position.y = initialY + App->stage05->spawnPos.y;
+	position.x = (int)App->stage05->spawnPos.x + initialX;
+	position.y = (int)App->stage05->spawnPos.y + initialY;
 }
 
