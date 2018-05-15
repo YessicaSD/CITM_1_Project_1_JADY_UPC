34d519bb3a2391d968afe5c5c7d6c96e27912e6c
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2018-05-15 20:56:17 +0200

    hidding ui

--- LastResort/ModuleStage05.cpp
+++ LastResort/ModuleStage05.cpp
@@ -141,6 +141,7 @@ bool Module5lvlScene::Start()
 	//App->enemies->AddEnemy(REDBATS, 758, 25);
 	//App->enemies->AddEnemy(OSCILATOR, 500, 0);
 	App->enemies->AddEnemy(BASIC, 250, 10, 1000);
+	App->enemies->AddEnemy(OSCILATOR, -47, 470);
 	App->enemies->AddEnemy(FRONT_TURRET, 47, 64, 0, POWERUP_TYPE::NOPOWERUP);
 
 	//Colliders--------------------------------------------------------------------------------------------------
