e22cafcf0cb90687765cb8d3da3503bdd3eb26db
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2018-05-16 21:56:42 +0200

    adding outdoor turret

---
+++ LastResort/Outdoor_turret.h
@@ -0,0 +1,18 @@
+//#ifndef __ENEMY_BASIC_H__
+//#define __ENEMY_BASIC_H__
+//
+//#include "Enemy.h"
+//
+//class Enemy_Basic : public Enemy
+//{
+//private:
+//	Animation basicenemy;
+//	int initialY;
+//
+//public:
+//	Enemy_Basic(int x, int y, POWERUP_TYPE pu_t);
+//	void Move();
+//};
+//
+//#endif // __ENEMY_BASIC_H__
+//
