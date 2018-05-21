8eabfa0227dcc5846d4a046eeebd8a93783d50ea
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2018-05-21 23:49:05 +0200

    adding big asteroid

--- LastResort/Enemy_Big_Asteriod.h
+++ LastResort/Enemy_Big_Asteriod.h
@@ -7,13 +7,6 @@ class Enemy_Big_Asteroid : public Enemy
 {
 private:
 	Animation BigAsteroid;
-	fPoint finalPosition;
-	bool going_right;
-	bool going_left;
-	SDL_Texture *LacerTex;
-	int resp;
-	int limit;
-	int limit_2;
 public:
 	Enemy_Big_Asteroid(int x, int y, POWERUP_TYPE pu_t);
 	~Enemy_Big_Asteroid() {};
