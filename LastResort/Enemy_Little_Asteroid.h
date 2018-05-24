71dd69e4fdb070ef5afe9a7fba5e47139bb31f10
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2018-05-24 12:54:38 +0200

    adding little asteroid

---
+++ LastResort/Enemy_Little_Asteroid.h
@@ -0,0 +1,20 @@
+#ifndef __ENEMY_LITTLE_ASTERIOD_H__
+#define __ENEMY_LITTLE_ASTERIOD_H__
+
+#include "Enemy.h"
+#include "ModuleParticles.h"
+class Enemy_Little_Asteroid : public Enemy
+{
+private:
+	Animation LittleAsteroid;
+public:
+	Enemy_Little_Asteroid(int x, int y, POWERUP_TYPE pu_t);
+	~Enemy_Little_Asteroid() {};
+	void Move();
+	void Draw(SDL_Texture* sprites);
+	void OnCollision(Collider*);
+
+};
+
+#endif // __ENEMY_LITTLE_ASTERIOD_H__
+
