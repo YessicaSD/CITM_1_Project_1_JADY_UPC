bc223c91ba9bd4d850e30999058de05559a20d2d
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2018-06-01 21:36:10 +0200

    adding laser

--- LastResort/Particle_Indoor_Laser.cpp
+++ LastResort/Particle_Indoor_Laser.cpp
@@ -5,27 +5,29 @@
 
 Particle_Indoor_Laser::Particle_Indoor_Laser(Particle& p, fPoint position, fPoint speed, Uint32 delay, COLLIDER_TYPE colType, SDL_Texture* tex) :Particle(p, position, speed, delay, colType, tex)
 {
+	
 	idleAnimation=p.anim.GetFrame() ;
 	animation = { 221,0,0,6 };
-	LOG("ANIM.X %i", anim.GetCurrentFrame().x);
+	
 	
 }
 void Particle_Indoor_Laser::Move()
 {
+	position.x = App->stage05->spawnPos.x + fixedPos.x;
+	position.y = App->stage05->spawnPos.y + fixedPos.y;
 	if (stateLaser != DECREASE)
 	{
 		fixedPos += speed;
-		position.x = App->stage05->spawnPos.x + fixedPos.x;
-		position.y = App->stage05->spawnPos.y + fixedPos.y;
+		
 	}
 
 	if (stateLaser == INCREASE)
 	{
-		LOG("ANIM.X %i", anim.GetCurrentFrame().x);
+		
 		if (animation.x > idleAnimation.x)
 		{
-			animation.x -= 1;
-			animation.w += 1;
+			animation.x -= labs(speed.x);
+			animation.w += labs(speed.x);
 		}
 		else
 		{
@@ -34,7 +36,21 @@ void Particle_Indoor_Laser::Move()
 	}
 	if (stateLaser == IDLE)
 	{
-
+		if (speed.x > 0)
+		{
+			if (position.x >= App->stage05->spawnPos.x + 850)
+				stateLaser = DECREASE;
+		}
+		else
+		{
+			if (position.x <= App->stage05->spawnPos.x + 620)
+				stateLaser = DECREASE;
+		}
+		
+	}
+	if (stateLaser == DECREASE)
+	{
+		animation.w -= labs(speed.x);
 	}
 }
 void Particle_Indoor_Laser::Draw()
