1120e72da4d60f4cbe668838e739270159da8fb1
AAurelioGN <36158509+alejandro61299@users.noreply.github.com>
2018-06-02 04:48:35 +0200

    Updated boss enemy

--- LastResort/Enemy_Boss_05.h
+++ LastResort/Enemy_Boss_05.h
@@ -1,1 +1,26 @@
-#pragma once
+#ifndef __ENEMY_INTERGSLSCTIC_F_H__
+#define __ENEMY_INTERGSLSCTIC_F_H__
+
+#include "Enemy.h"
+#include "MovPath.h"
+
+class ModulePlayer;
+
+class Enemy_Boss_05 : public Enemy
+{
+public:
+	Enemy_Boss_05(int x, int y, float hp, int scoreValue, POWERUP_TYPE powerupDrop);
+	void Move();
+	void Draw(SDL_Texture* sprites);
+
+private:
+	//Animations---------------------------
+	Animation bossAnim;
+	//Variables----------------------------
+	fPoint speed;
+	fPoint aceleration;
+	int shootFrames;
+};
+
+#endif // __ENEMY_INTERGSLSCTIC_F_H__
+
